76bb7b7e51a78e8db3c4cd84149deb74be83841d
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-09-18 19:49:48 +0200

    Add plugin loader
    Function looks for subfolders in a given path. If subfolder includes
    a file - by default `__init__.py` - it will get loaded. Expected entry
    point is a `run` function.

---
+++ findc/toolchain/loader.py
@@ -0,0 +1,23 @@
+"""
+https://lkubuntu.wordpress.com/2012/10/02/writing-a-python-plugin-api/
+"""
+
+import imp
+import os
+import click
+
+def get_plugins(out, folder=None, desc=None, main=None):
+    folder = folder or "./plugins"
+    desc = desc or folder
+    main = main or "__init__"
+    plugins = {}
+    files = os.listdir(folder)
+    for i in files:
+        location = os.path.join(folder, i)
+        if not os.path.isdir(location) or not main + ".py" in os.listdir(location):
+            continue
+        info = imp.find_module(main, [location])
+        plugin = imp.load_module(main, *info)
+        plugins[i] = plugin
+        out.trace("Registering %s plugin: %s" % (click.style(desc, fg="yellow"), click.style(i, fg="green")))
+    return plugins
