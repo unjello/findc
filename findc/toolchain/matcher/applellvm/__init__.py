dae50cf7030b7f260cb94c5966a103b416c73123
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-09-19 22:37:27 +0200

    Return meta information for toolchain

--- findc/toolchain/matcher/applellvm/__init__.py
+++ findc/toolchain/matcher/applellvm/__init__.py
@@ -4,7 +4,7 @@ from subprocess import check_output
 _command_candidate_patterns = ['/usr/bin/gcc','/usr/bin/g++', '/usr/bin/clang', '/usr/bin/clang++']
 _find_apple_llvm_version='^Apple LLVM version ([0-9\.]+)'
 
-def _detect_apple_clang(command, out=None):
+def _detect_apple_llvm(command, out=None):
   if not command:
     return None
 
@@ -22,15 +22,16 @@ def _detect_apple_clang(command, out=None):
 
   out.info("Found Apple LLVM compiler version %s in %s" % (match.group(1), command))
   meta = {
-    "tool": "clang",
-    "path": "/usr/bin/clang",
+    "tool": "applellvm",
+    "path": command,
     "version": match.group(1),
     "raw_version_output": output
   }
+  return meta
 
 def run(command, out=None):
   result = []
-  for f in [_detect_apple_clang]:
+  for f in [_detect_apple_llvm]:
     ret = f(command, out)
     if ret:
       result.append(ret)
