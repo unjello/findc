15889abd0d7bf4692f3d8e14c648afcfc6e9f086
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-09-21 14:40:34 +0200

    Add YAML output formatting

--- findc/toolchain/printer.py
+++ findc/toolchain/printer.py
@@ -1,6 +1,30 @@
-_print_format='{:25}{:15}{:10}'
+import yaml
 
-def print_toolchains(meta, output):
+_print_format='{:25}{:15}{:10}'
+_allowed_keys = ['path', 'tool', 'version']
+
+def _fiter_out_meta(meta):
+  result = []
+  for m in meta:
+    result.append({k:v for k,v in m.iteritems() if k in _allowed_keys})
+  return result
+
+def _output_plain(meta):
   print(_print_format.format('PATH', 'TOOLCHAIN', 'VERSION'))
   for m in meta:
-    print(_print_format.format(m[0]['path'], m[0]['tool'], m[0]['version']))
+    print(_print_format.format(m['path'], m['tool'], m['version']))
+
+def _output_yaml(meta):
+  print yaml.dump(meta, default_flow_style=False)
+
+def _output_json(meta):
+  pass
+
+def print_toolchains(meta, output):
+  meta = _fiter_out_meta(meta)
+  if output == "yaml":
+    _output_yaml(meta)
+  elif output == "json":
+    _output_json(meta)
+  else:
+    _output_plain(meta)
