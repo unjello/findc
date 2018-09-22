27f2ce940f1849975570477dbb6a16ab0c42058c
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-09-22 20:31:20 +0200

    Refactor Apple LLVM toolchain methods

--- findc/toolchain/matcher/applellvm/__init__.py
+++ findc/toolchain/matcher/applellvm/__init__.py
@@ -5,23 +5,8 @@ _command_candidate_patterns = ['/usr/bin/gcc','/usr/bin/g++', '/usr/bin/clang', 
 _find_apple_llvm_version='^Apple LLVM version ([0-9\.]+)'
 _find_apple_llvm_options='\s+--?([A-Za-z0-9#\-\+]+)((([\s,]*)|=)<([^>]+)>)?\s*([^\n]+)'
 
-def _detect_apple_llvm(command, out=None):
-  if not command:
-    return None
 
-  if command not in _command_candidate_patterns:
-    if out:
-      out.trace("%s is not valid command candidate" % command)
-    return None
-
-  output = check_output([command, "--version"], stderr=STDOUT)
-  match = re.search(_find_apple_llvm_version, output)
-  if not match:
-    if out:
-      out.trace("%s is not Apple LLVM compiler" % command)
-    return None
-  version = match.group(1)
-
+def _detect_apple_llvm_options(command):
   output = check_output([command, "--help"], stderr=STDOUT)
   match = re.findall(_find_apple_llvm_options, output)
   options = []
@@ -31,6 +16,32 @@ def _detect_apple_llvm(command, out=None):
       "paremeter": m[4],
       "description": m[5]
     })
+  return options
+
+def _detect_apple_llvm_version(command):
+  output = check_output([command, "--version"], stderr=STDOUT)
+  match = re.search(_find_apple_llvm_version, output)
+  if not match:
+    if out:
+      out.trace("%s is not Apple LLVM compiler" % command)
+    return None
+  return match.group(1)
+
+def _detect_apple_llvm(command, out=None):
+  if not command:
+    return None
+
+  if command not in _command_candidate_patterns:
+    if out:
+      out.trace("%s is not valid command candidate" % command)
+    return None
+
+  
+  version = _detect_apple_llvm_version(command)
+  if not version:
+    return None
+
+  options = _detect_apple_llvm_options(command)
 
   out.info("%s: found Apple LLVM compiler version %s" % (command, version))
   meta = {
