13311fba15368465b801bc328093f3603a35c4fa
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-09-23 10:32:32 +0200

    Fix improperly loaded modules
    Modules where calling `load_module` with the same name of `__init__`.
    This name needs to be unique, so when `clang` module was called first,
    and `applellvm` second, under the same name it actually caused
    `clang` module under name of `__init__` to reload as `applellvm`.
    Ideally, this name will need to include parent package, such as
    `matchers` if more plugins will come, but for now, it's enough.

--- findc/toolchain/loader.py
+++ findc/toolchain/loader.py
@@ -17,7 +17,8 @@ def get_plugins(out, folder=None, desc=None, main=None):
         if not os.path.isdir(location) or not main + ".py" in os.listdir(location):
             continue
         info = imp.find_module(main, [location])
-        plugin = imp.load_module(main, *info)
+        plugin = imp.load_module(i, *info)
         plugins[i] = plugin
         out.trace("Registering %s plugin: %s" % (click.style(desc, fg="yellow"), click.style(i, fg="green")))
+    print plugins
     return plugins
