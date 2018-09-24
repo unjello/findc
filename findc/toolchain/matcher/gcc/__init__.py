a28f8fa33e1fad812138d15ba50112c0ae99b1ac
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-09-24 22:58:09 +0200

    Fix gcc command matching pattern
    Pattern of `gcc-[A-Za-z0-9]+` was also mattching commands like
    `gcc-ar` or `gcc-nm`. Change pattern so that first character after
    a dash needs to be a digit.

--- findc/toolchain/matcher/gcc/__init__.py
+++ findc/toolchain/matcher/gcc/__init__.py
@@ -3,7 +3,7 @@ from subprocess import check_output, STDOUT
 
 # [^a-zA-Z] before g++ patterns is there to exclude clang++ from matching, so that /g++ or \g++.exe can only match.
 # it could be done by splitting path, and taking only last part
-_command_candidate_patterns = ['gcc(\.exe)?$','gcc-[A-Za-z0-9]+(\.exe)?$', '[^a-zA-z]g\+\+(\.exe)?$', '[^a-zA-z]g\+\+-[A-Za-z0-9]+(\.exe)?$']
+_command_candidate_patterns = ['gcc(\.exe)?$','gcc-[0-9]{1}[A-Za-z0-9]*(\.exe)?$', '[^a-zA-z]g\+\+(\.exe)?$', '[^a-zA-z]g\+\+-[0-9]{1}[A-Za-z0-9]*(\.exe)?$']
 _apple_llvm_pattern='Apple LLVM version ([0-9\.]+)'
 _gcc_version_pattern='g(cc|\+\+)[^(]+\([^)]+\)\s+(\d+\.\d+\.\d+)'
 
