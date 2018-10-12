6f94292f674f1ddf310d961a280e9853c349fb1d
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-10-12 20:58:38 +0200

    Fix module import paths

--- find_compiler/findc.py
+++ find_compiler/findc.py
@@ -1,7 +1,8 @@
 import click
 import six
-from output import Out
-from toolchain.detect import detect_toolchains
+from find_compiler.output import Out
+from find_compiler.toolchain.detect import detect_toolchains
+from find_compiler import __version__ as findc_version
 
 @click.command()
 @click.option('-o', '--output', type=click.Choice(['plain', 'yaml', 'json']), default='plain', help='Output formatting')
@@ -11,7 +12,7 @@ def main(output, verbose):
     CLI for finding suitable C/C++ compiler on current platform
     """
     out = Out(output, verbose)
-    out.write("findc %s" % "0.0.4", bold=True)
+    out.write("findc %s" % findc_version, bold=True)
 
     from toolchain.loader import get_plugins
     from find import find_match_in_path
@@ -31,5 +32,7 @@ def main(output, verbose):
 
 if __name__ == '__main__':
     from os import sys, path
-    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+    module_dir = path.dirname(path.dirname(path.abspath(__file__)))
+    sys.path.append(module_dir)
+    sys.path.append(path.dirname(module_dir))
     main()
