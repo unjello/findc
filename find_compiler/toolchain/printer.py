3138ebbcf1444ea72bfb331fb2248ed7c5cd6ee3
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-10-12 21:19:48 +0200

    Add dynamic resizing of column widths in plain mode

--- find_compiler/toolchain/printer.py
+++ find_compiler/toolchain/printer.py
@@ -1,7 +1,7 @@
 import yaml
 import json
 
-_print_format='{:25}{:15}{:10}'
+_print_format='{{:{}}}{{:{}}}{{:{}}}'
 _allowed_keys = ['path', 'tool', 'version', 'options']
 
 def _fiter_out_meta(meta):
@@ -10,10 +10,21 @@ def _fiter_out_meta(meta):
     result.append({k:v for k,v in m.items() if k in _allowed_keys})
   return result
 
-def _output_plain(meta):
-  print(_print_format.format('PATH', 'TOOLCHAIN', 'VERSION'))
+def _adjust_print_format(meta):
+  path_size = 25
+  tool_size = 15
+  version_size = 10
   for m in meta:
-    print(_print_format.format(m['path'], m['tool'], m['version']))
+    path_size = max(len(m['path']) + 3, path_size)
+    tool_size = max(len(m['tool']) + 3, tool_size)
+    version_size = max(len(m['version']) + 3, version_size)
+  return _print_format.format(path_size, tool_size, version_size)
+
+def _output_plain(meta):
+  fmt = _adjust_print_format(meta)
+  print(fmt.format('PATH', 'TOOLCHAIN', 'VERSION'))
+  for m in meta:
+    print(fmt.format(m['path'], m['tool'], m['version']))
 
 def _output_yaml(meta):
   print(yaml.dump(meta, default_flow_style=False))
