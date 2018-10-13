b0ff70c48320ba27c69ff871de721e19baaa2fc7
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-10-13 20:32:14 +0200

    Add MSVC to search path

--- find_compiler/find.py
+++ find_compiler/find.py
@@ -1,10 +1,23 @@
 import os
 import glob
 
+# Visual Studio by default does not put compiler in PATH. It relies on batch
+# scripts to properly set up environment, so there's high chance we will need
+# too find C/C++ compiler looking through well-known toolchain folders.
+# FIXME: supplying extra paths should probably be merged into "plugins"
+msvc_search_path = [
+  # Visual Studio 2017:
+  "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\VC\\Tools\\MSVC\\14.15.26726\\bin\\Hostx64\\x64",
+  "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\VC\\Tools\\MSVC\\14.15.26726\\bin\\Hostx64\\x86",
+  "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\VC\\Tools\\MSVC\\14.15.26726\\bin\\Hostx86\\x64",
+  "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\VC\\Tools\\MSVC\\14.15.26726\\bin\\Hostx64\\x86"
+]
+
 def find_match_in_path(name, path=None):
   path = path or os.environ.get('PATH')
   results = []
-  for dir in path.split(os.pathsep):
+  paths = path.split(os.pathsep) + msvc_search_path
+  for dir in paths:
     matches = glob.glob(os.path.join(dir, name))
     results.extend(matches)
   return results
--- find_compiler/toolchain/detect.py
+++ find_compiler/toolchain/detect.py
@@ -2,6 +2,8 @@ from find_compiler.find import find_match_in_path
 
 def detect_toolchains(out):
   tools = []
-  for f in ['clang', 'clang++', 'clang-*', 'clang++-*', 'gcc', 'gcc-*', 'g++', 'g++-*']:
+
+  # FIXME: supplying those preliminary patterns should probably be merged into "plugins"
+  for f in ['clang', 'clang++', 'clang-*', 'clang++-*', 'gcc', 'gcc-*', 'g++', 'g++-*', 'cl.exe']:
     tools.extend(find_match_in_path(f))
   return tools
